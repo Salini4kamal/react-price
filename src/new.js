ab6746519a7b5107cc7a6d08875989c952778464
salini4kamal <salini4kaml@gmail.com>
2023-04-05 20:40:54 +0530

    first commit

---
+++ src/new.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+export 
+
+
+//  <FontAwesomeIcon icon={faCoffee} />
+
+// ReactDOM.render( document.body)
